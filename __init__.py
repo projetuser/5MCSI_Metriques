6a5584b64b4ec4e6114073d6a1d518e2afffbb29
Sushi <71588514+projetuser@users.noreply.github.com>
2025-04-01 14:30:27 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -11,10 +11,6 @@ app = Flask(__name__)
 def hello_world():
     return render_template('hello.html')
 
-@app.route("/contact/")
-def MaPremiereAPI():
-    return "<h2>Ma page de contact</h2>"
-
 @app.route('/tawarano/')
 def meteo():
     response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
